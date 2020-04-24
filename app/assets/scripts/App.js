edc958b2a62020a55e9de038b006987686163770
Cheer <cripaktcheer@gmail.com>
2020-04-24 20:27:49 +0800

    famous-writers done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import '../styles/styles.css';
 import $ from 'jquery';
-import lightbox from 'lightbox2'
+import lightbox from 'lightbox2';
+
 
 import RevealOnScroll from './modules/RevealOnScroll'
 
@@ -11,7 +12,7 @@ if (module.hot) {
 
 /* 
 * In fact I prefer document.querySelector. 
-* Here's just a trivial showcase for using jQuery :)
+* Here's just a simple showcase for using jQuery :)
 */
 
 $(document).ready(function() {
@@ -27,5 +28,15 @@ $(document).ready(function() {
   */
   new RevealOnScroll(document.querySelectorAll('.gallery__img--row-one'),75);
   new RevealOnScroll(document.querySelectorAll('.gallery__img--row-two'),75);
+
+
+  $('.writers-accordion').click(function(event) {
+        
+    if(event.target.id.split('-')[0] === 'button') {
+       $('#book-1').attr('src', 'assets/images/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+       $('#book-2').attr('src', 'assets/images/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+    }
+
+  });
 
 });
